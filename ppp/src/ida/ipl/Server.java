dc08b737a99bb24734c439deda17d9d51c59a232
Dunky13 <mwent@live.nl>
2015-07-10 18:05:44 +0200

    test2

--- ppp/src/ida/ipl/Server.java
+++ ppp/src/ida/ipl/Server.java
@@ -211,7 +211,7 @@ public class Server implements MessageUpcall, ReceivePortConnectUpcall
 
 			synchronized (initialBoard)
 			{
-				int bound = initialBoard.bound();
+				int bound = initialBoard.bound() + 1;
 				initialBoard.setBound(bound);
 				System.out.print(" " + bound);
 			}
